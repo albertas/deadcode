fcecda02347e3e34e208cdb865ddfb7edb4033a5
Albertas Gimbutas <albertasgim@gmail.com>
2023-09-30 02:12:16 +0300

    Add an example for more accurate usage detection

---
+++ deadcode/tests/test_accurate_usage_detection.py
@@ -0,0 +1,50 @@
+from unittest import skip
+
+from deadcode.cli import main
+from deadcode.tests.base import BaseTestCase
+
+
+@skip
+class TestAccurateUsageDetection(BaseTestCase):
+    def test_function_call_with_one_argument(self):
+        ## TODO:
+        # Variable types should be tracked in each scope
+        # When scope changes during call: parameter types should be mapped from arguments
+
+        # When new type is defined: during import or definition: scope has to be updated.
+
+
+        self.files = {
+            "foo.py": """
+                class X:
+                    def used_method(self):
+                        pass
+
+                x = X()
+
+                def foo(y):
+                    y.used_method()
+
+                foo(x)
+                """
+        }
+
+        unused_names = main(["foo.py", "--no-color", "--fix"])
+        self.assertIsNone(unused_names)
+
+        self.assertFiles(
+            {
+                "foo.py": """
+                    class X:
+                        def used_method(self):
+                            pass
+
+                    x = X()
+
+                    def foo(y):
+                        y.used_method()
+
+                    foo(x)
+                    """
+            }
+        )
